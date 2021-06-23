1a4af6914a6e0d393e142ccad2a08106d174921d
K0infig <71273189+K0infig@users.noreply.github.com>
2021-06-23 15:03:10 +0530

    Update code.py

--- code.py
+++ code.py
@@ -119,3 +119,15 @@ fig.add_trace(go.Scatter(x=[third_std_deviation_end,third_std_deviation_end], y=
 fig.show()
 
 
+df = pd.read_csv("data1.csv")
+data = df["Math_score"].tolist()
+mean_of_sample3 = statistics.mean(data)
+print("mean of sample3:- ",mean_of_sample3)
+fig = ff.create_distplot([mean_list], ["student marks"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[mean_of_sample3, mean_of_sample3], y=[0, 0.17], mode="lines", name="MEAN OF STUDNETS WHO GOT FUNSHEETS"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
+fig.show()
+
+
